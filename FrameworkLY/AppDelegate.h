12a10c0a5395f137191645809b4a4f8368bec9ed
ly <1364757394@qq.com>
2017-05-15 09:55:40 +0800

    Initial Commit

---
+++ FrameworkLY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FrameworkLY
+//
+//  Created by 李勇 on 2017/5/15.
+//  Copyright © 2017年 ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FrameworkLY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FrameworkLY
+//
+//  Created by 李勇 on 2017/5/15.
+//  Copyright © 2017年 ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
